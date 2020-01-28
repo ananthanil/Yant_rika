ff578420a3fced8af39ef139316b9fd7de923109
Ananth krishnan A <55017917+ananthanil@users.noreply.github.com>
2020-01-28 11:51:23 +0530

    imporoved registeration

--- public.py
+++ public.py
@@ -10,10 +10,29 @@ def index():
 		u_number=request.form['c_mobile']
 		u_email=request.form['c_email']
 
-		q="insert into reg values(null,'%s','%s','%s','%s',curdate())"%(u_name,u_collegename,u_number,u_email)
-		insert(q)
-
+		q = "select * from reg where phone = '%s' or email = '%s'"%(u_name, u_email)
+		existing = select(q)
 		
+		if len(existing) >= 1:
+			id= existing[0]['reg_id']
+			res = {
+				"success": False,
+				"message": "You are already registered with registeration id " + str(id),
+			}
+
+			return render_template('register.html', data= res)
+		else:
+			q="insert into reg values(null,'%s','%s','%s','%s',curdate())"%(u_name,u_collegename,u_number,u_email)
+			id = insert(q)
+
+			res = {
+				"success": True,
+				"message": "You are registered successfuly with registeration id " + str(id),
+			}
+
+			return render_template('register.html', data= res)
+
+
 
 
 	return render_template('index.html')
